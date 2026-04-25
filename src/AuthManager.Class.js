03fff09ea583cc2ae8920720c6106c3f88d05a7c
Fernando Luna <48518539+mobilepadawan@users.noreply.github.com>
2026-04-24 23:45:18 -0300

    Improving AuthManager

--- src/AuthManager.Class.js
+++ src/AuthManager.Class.js
@@ -2,11 +2,20 @@ import { collection, query, where, getDocs } from "firebase/firestore"
 import { db } from "./firebase.js"
 
 export class AuthManager {
-    static version = "1.0.0"
+    static version = "1.0.1"
     static copyright = "Fernando Omar Luna"
 
     static createToken() {
         return crypto.randomUUID().replaceAll("-", "")
+    }
+
+    static createLimitedToken(totalChars) {
+        if (typeof Number(totalChars) === 'number' && Number(totalChars) <= 32) {
+            const tokenCreated = crypto.randomUUID().replaceAll("-", "")
+            return tokenCreated.slice(0, parseInt(totalChars))
+        } else {
+            console.error("Error creating token:", totalChars)
+        }
     }
 
     static async validateUserByToken(tokenId) {
